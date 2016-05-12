7e4616bf5042b11534925cbb1aba270af601182a
Shankar Nakai <shankar.nakai@gmail.com>
2016-05-12 10:03:20 -0300

    Fazendo codigo para receber as informações do controle remoto

--- P2P_Cart.ino
+++ P2P_Cart.ino
@@ -16,12 +16,8 @@
 
 #include <IRremote.h>
 #include <IRremoteInt.h>
-#include <Stepper.h>
-#include <Servo.h>
 
 //INSTANCIAS DE OBJETOS 
-Stepper        motor         = Stepper(PASSOS_POR_PERIODO, MOTOR_PIN_1, MOTOR_PIN_2, MOTOR_PIN_3, MOTOR_PIN_4); 
-Servo          servo;
 IRrecv         receptor(RECEPTOR_PIN);  
 decode_results resultado;  
 
@@ -33,36 +29,17 @@ void setup() {
   Serial.begin(9600);  
   receptor.enableIRIn();
   //==============================================================
-
-  motor.setSpeed(MOTOR_VELOCIDADE);
-  servo.attach(SERVO_PIN);
 }
 
 void loop() {
   resposta = get_response();
 
   if (resposta > 0) {  
-
-    if (is_forward(resposta)) {
-      go_forward();
-    } 
-    else if (is_backward(resposta)) {  
-      go_backward();
-    }  
-
-    if (is_right(resposta)) {  
-      go_right();
-    }  
-    else if (is_left(resposta)) {  
-      go_left();
-    }  
-
     receptor.resume(); //Le o próximo valor  
   }
 
   delay(15);  
 }
-
 
 /**
  * Pega a resposta do controle remoto do KIT arduino.
@@ -73,88 +50,13 @@ float get_response() {
   float valor = 0;
 
   if (receptor.decode(&resultado)) {
+    //Create log do valor ligo pelo controle remoto
     Serial.print("Valor lido : ");
     Serial.println(resultado.value, HEX); 
 
+    //Guarda valor ligo na variável valor
     valor = resultado.value;  
   }
 
   return valor;
 }
-
-/**
- * Verifica se o valor passado corresponde a constante de seguir em frente
- * @since 0.0.1
- * @return int Retorna true/false 
- */
-boolean is_forward(float valor) {
-  return (valor == FRONT);
-}
-
-/**
- * Verifica se o valor passado corresponde a constante de voltar
- * @since 0.0.1
- * @param float valor Campos preenchido com um valor Hexdecimanl
- * @return int Retorna true/false 
- */
-boolean is_backward(float valor) {
-  return (valor == BACK);
-}
-
-/**
- * Verifica se o valor passado corresponde a virar para a direita
- * @since 0.0.1
- * @param float valor Campos preenchido com um valor Hexdecimanl
- * @return int Retorna true/false 
- */
-boolean is_right(float valor) {
-  return (valor == RIGHT);
-}
-
-/**
- * Verifica se o valor passado corresponde a virar para a esquerda
- * @since 0.0.1
- * @param float valor Campos preenchido com um valor Hexdecimanl
- * @return int Retorna true/false 
- */
-boolean is_left(float valor) {
-  return (valor == LEFT);
-}
-
-/**
- * Move o carrinho para frente
- * @since 0.0.1
- * @return void
- */
-void go_forward() {
-  motor.step(MOTOR_STEP *(-1));
-}
-
-/**
- * Move o carrinho para tras
- * @since 0.0.1
- * @return void
- */
-void go_backward() {
-  motor.step(MOTOR_STEP);
-}
-
-/**
- * Move o carrinho para direita
- * @since 0.0.1
- * @return void
- */
-void go_right() {
-  servo.write(SERVO_MIN_TURN);
-}
-
-/**
- * Move o carrinho para esquerda
- * @since 0.0.1
- * @return void
- */
-void go_left(){
-  servo.write(SERVO_MAX_TURN);
-}
-
-
